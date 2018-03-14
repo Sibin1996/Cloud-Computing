2b939253a4656351920b804fe27821e968293a98
Sibin Kartik <sibink.tiwari@st.niituniversity.in>
2018-03-14 23:14:15 +0530

    UpdateDirectory

---
+++ UpdateDirectory.cs
@@ -0,0 +1,31 @@
+sing System;
+using System.IO;
+using net.openstack.Core.Domain;
+using net.openstack.Providers.Rackspace;
+using Rackspace.CloudNetworks;
+using Rackspace.RackConnect;
+
+namespace CSharpProgram2
+{
+    public class DirectoryRoot
+    {
+        public static void Main()
+        {
+
+            string dir = "C:\\Users\\user\\Desktop\\Directory";
+            try
+            {
+                //Set the current directory.
+                Directory.SetCurrentDirectory(dir);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("The specified directory does not exist. {0}", e);
+            }
+            // Print to console the results.
+            Console.WriteLine("Root directory: {0}", Directory.GetDirectoryRoot(dir));
+            Console.WriteLine("Current directory: {0}", Directory.GetCurrentDirectory());
+        }
+    }
+}
+
